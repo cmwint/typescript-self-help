5423626c6d1c7047315d57b951ee9a0a552c2498
Corinne Winthrop <corinne@bythepixel.com>
2017-12-20 13:56:31 -0700

    Add class

--- greeter.js
+++ greeter.js
@@ -1,12 +1,25 @@
 // TypeScript compiler:
 // tsc greeter.ts
+// class-based OOP
+// interfaces and classes (are supposed to) work well together
+// classes in TS are shorthand for the same OO used in reg JS
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 // type annotation ': string'
 // errors on comilation if input is not a string
 function greeter(person) {
     return "hello," + person.firstName + " " + person.lastName;
 }
-var user = {
-    firstName: "Jane",
-    lastName: "User"
-};
+// let user = {
+//     firstName: "Jane",
+//     lastName: "User"
+// };
+var user = new Student('Jane', 'M.', 'User');
 document.body.innerHTML = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,22 @@
 // TypeScript compiler:
 // tsc greeter.ts
 
+// class-based OOP
+// interfaces and classes (are supposed to) work well together
+
+// classes in TS are shorthand for the same OO used in reg JS
+class Student {
+    fullName: string;
+    constructor (
+        public firstName: string,
+        public middleInitial: string,
+        public lastName: string
+    ){
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+
 // interfaces describes object's structure
 interface Person {
     firstName: string;
@@ -13,9 +29,10 @@ function greeter(person: Person) {
     return "hello," + person.firstName + " " + person.lastName;
 }
 
-let user = {
-    firstName: "Jane",
-    lastName: "User"
-};
+// let user = {
+//     firstName: "Jane",
+//     lastName: "User"
+// };
+let user = new Student('Jane', 'M.', 'User');
 
 document.body.innerHTML = greeter(user);
