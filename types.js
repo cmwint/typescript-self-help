fceb39c1af76bf7778431da56b7385a66a679778
Corinne Winthrop <corinne@bythepixel.com>
2017-12-26 10:47:46 -0700

    type system

---
+++ types.js
@@ -0,0 +1,5 @@
+// define a type with a colon
+// types are important during compile time, not with JS is runnning
+// to check for errors
+var n = 1;
+n = "Name";
---
+++ types.ts
@@ -0,0 +1,9 @@
+// define a type with a colon
+// types are important during compile time, not with JS is runnning
+// to check for errors
+var n : any = 1;
+
+n = "Name";
+
+// avoid using the "any" type
+// the stricter you are with types, the more errors you are able to prevent
