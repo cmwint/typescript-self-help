967f20f9edaed1f940efdbc3c40b448a02b2674c
Corinne Winthrop <corinne@bythepixel.com>
2017-12-26 11:06:01 -0700

    Understanding more types

--- types.js
+++ types.js
@@ -3,3 +3,34 @@
 // to check for errors
 var n = 1;
 n = "Name";
+// avoid using the "any" type
+// the stricter you are with types, the more errors you are able to prevent
+var isWinter = false;
+// isWinter = 123;
+var count = 5;
+var something = "Hello";
+// vector = array that can only contain one type of variable
+// array that only contains strings
+var names = ["Bill", "John", "Bob"];
+// change array type to any, to allow more than one type
+//var names: string[] = ["Bill", "John", "Bob", 4, 10];
+// enum = easy way to store built-in varaibles
+var Starks;
+(function (Starks) {
+    Starks[Starks["Bill"] = 0] = "Bill";
+    Starks[Starks["John"] = 1] = "John";
+    Starks[Starks["Bob"] = 2] = "Bob";
+})(Starks || (Starks = {}));
+;
+// can be referenced throughout
+var bill = Starks.Bill;
+// the type the function returns is after the parentheses but before the curly brackets
+// the function getName() returns a string
+function getName() {
+    return "John";
+}
+// void is not any type
+// it's usually used to denote nothing returned in a function
+function returnConsole() {
+    console.log('A message here');
+}
--- types.ts
+++ types.ts
@@ -7,3 +7,35 @@ n = "Name";
 
 // avoid using the "any" type
 // the stricter you are with types, the more errors you are able to prevent
+
+
+
+var isWinter: boolean = false;
+// isWinter = 123;
+
+var count: Number = 5;
+var something: String = "Hello";
+
+// vector = array that can only contain one type of variable
+// array that only contains strings
+var names: string[] = ["Bill", "John", "Bob"];
+
+// change array type to any, to allow more than one type
+//var names: string[] = ["Bill", "John", "Bob", 4, 10];
+
+// enum = easy way to store built-in varaibles
+enum Starks {Bill,John,Bob};
+// can be referenced throughout
+var bill: Starks = Starks.Bill;
+
+// the type the function returns is after the parentheses but before the curly brackets
+// the function getName() returns a string
+function getName() : string {
+    return "John";
+}
+
+// void is not any type
+// it's usually used to denote nothing returned in a function
+function returnConsole() : void {
+    console.log('A message here');
+}
