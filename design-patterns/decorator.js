38c1a8913c3ec1058fae862e0be5d715f3fbf1f5
Corinne Winthrop <corinne@bythepixel.com>
2017-12-26 15:58:38 -0700

    Compile into js

---
+++ design-patterns/decorator.js
@@ -0,0 +1,75 @@
+// when you want to allow objects to have different options
+// the pattern, not the feature
+// example: the options decorate your car, hence the name of the pattern
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// take a base class, instantiate it, then wrap many options around it
+// creating abstract class, since there should never be just the class of "car"
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    Car.prototype.getDescription = function () {
+        return this.description;
+    };
+    return Car;
+}());
+var ModelS = /** @class */ (function (_super) {
+    __extends(ModelS, _super);
+    function ModelS() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Model S";
+        return _this;
+    }
+    ModelS.prototype.cost = function () {
+        return 73000;
+    };
+    return ModelS;
+}(Car));
+var ModelX = /** @class */ (function (_super) {
+    __extends(ModelX, _super);
+    function ModelX() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.description = "Model X";
+        return _this;
+    }
+    ModelX.prototype.cost = function () {
+        return 77000;
+    };
+    return ModelX;
+}(Car));
+// then define a class for all of our options that we can choose from
+var CarOptions = /** @class */ (function (_super) {
+    __extends(CarOptions, _super);
+    function CarOptions() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return CarOptions;
+}(Car));
+// vscode will throw and error saying I need to implement some elements of the extended class
+var EnhancedAutoPilot = /** @class */ (function (_super) {
+    __extends(EnhancedAutoPilot, _super);
+    // need to fill in decoratedCar property
+    // so give it an instance of type car
+    function EnhancedAutoPilot(car) {
+        var _this = _super.call(this) || this;
+        _this.decoratedCar = car;
+        return _this;
+    }
+    EnhancedAutoPilot.prototype.getDescription = function () {
+        // return the description of the car that we decorate, plus description of option
+        return this.decoratedCar.getDescription() + ', Enhanced AUto Pilot';
+    };
+    EnhancedAutoPilot.prototype.cost = function () {
+        // cost = cost of decorated car, plus cost of option
+        return this.decoratedCar.cost() + 5000;
+    };
+    return EnhancedAutoPilot;
+}(CarOptions));
