f71330755943d0e0818618b1664b00c6cab8702b
Corinne Winthrop <corinne@bythepixel.com>
2017-12-26 15:43:14 -0700

    Begin decorator learning

---
+++ design-patterns/decorator.ts
@@ -0,0 +1,15 @@
+class ModelS {
+    public description = "Model S";
+
+    public cost(): number {
+        return 73000;
+    }
+}
+
+class ModelX {
+    public description = "Model X";
+
+    public cost(): number {
+        return 77000;
+    }
+}
